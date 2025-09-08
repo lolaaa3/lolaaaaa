0722b9a7187c58b8272b400bfcec03134a3b1584
lolaaa3 <nj.mendezg1@uniandes.edu.co>
2025-09-08 22:45:19 +0000

    animalitos

--- script.js
+++ script.js
@@ -2,7 +2,8 @@
 const listaCabezas = [
   "../assets/img/cabeza1.jpg",
   "../assets/img/cabeza2.jpg",
-  "../assets/img/cabeza3.png",
+  "../assets/img/cabeza4.jpg",
+  "../assets/img/cabeza5.jpg",
 ];
 const listaTroncos = [
  "../assets/img/tronco1.jpg",
@@ -14,17 +15,24 @@ const listaPatas = [
   "../assets/img/patas2.jpg",
   "../assets/img/patas3.png",
 ];
+const listaSombrero = [
+  "../assets/img/sombrero.jpg",
+  "../assets/img/sombrero2.jpg",
+  
+];
 
 // Obtenemos los contenedores de las imágenes del HTML usando los IDs
 const cabeza = document.getElementById("cabeza");
 const tronco = document.getElementById("tronco");
 const patas = document.getElementById("patas");
+const sombrero = document.getElementById("sombrero")
 const boton = document.getElementById("boton");
 
 // Inicializamos las variables de los números aleatorios
 let cabezaAleatorio = 0;
 let troncoAleatorio = 0;
 let patasAleatorio = 0;
+let sombreroAleatorio = 0;
 
 // Función para generar un número aleatorio entre dos valores
 function numeroAleatorio(min, max) {
@@ -36,13 +44,15 @@ function generarBestia() {
   cabezaAleatorio = numeroAleatorio(0, listaCabezas.length);
   troncoAleatorio = numeroAleatorio(0, listaTroncos.length);
   patasAleatorio = numeroAleatorio(0, listaPatas.length);
+  sombreroAleatorio = numeroAleatorio(0, listaSombrero.length);
 
   // Asignamos la nueva fuente (source) a cada imagen
   cabeza.src = `${listaCabezas[cabezaAleatorio]}`; //ruta + listaCabezas[cabezaAleatorio];
   tronco.src = `${listaTroncos[troncoAleatorio]}`;
   patas.src = `${listaPatas[patasAleatorio]}`;
+  sombrero.src = `${listaSombrero[sombreroAleatorio]}`;
 
-  console.log(cabezaAleatorio, troncoAleatorio, patasAleatorio);
+  console.log(cabezaAleatorio, troncoAleatorio, patasAleatorio, sombreroAleatorio);
 }
 
 // Generamos un nuevo collage cada vez que hacemos click en el botón "mezclar"
